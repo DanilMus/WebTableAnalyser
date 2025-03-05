0b61097f5618407700f04fd5fede4dc12b8b4640
Danil <musikhin.danil@gmail.com>
2025-03-05 16:21:58 +0500

    Work started

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,42 @@
+<script setup>
+import TableChart from './components/TableChart.vue';
+</script>
+
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <div>
+        <h1> График данных</h1>
+        <TableChart />
+    </div>
+</template>
+
+
+
+
+
+
+<!-- <template>
+    <img alt="Vue logo" src="./assets/logo.png">
+    <HelloWorld msg="Welcome to Your Vue.js App"/>
 </template>
 
 <script>
 import HelloWorld from './components/HelloWorld.vue'
 
 export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
+    name: 'App',
+    components: {
+        HelloWorld
+    }
 }
 </script>
 
 <style>
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+    font-family: Avenir, Helvetica, Arial, sans-serif;
+    -webkit-font-smoothing: antialiased;
+    -moz-osx-font-smoothing: grayscale;
+    text-align: center;
+    color: #2c3e50;
+    margin-top: 60px;
 }
-</style>
+</style> -->
---
+++ src/components/TableChart.vue
@@ -0,0 +1,23 @@
+<script setup>
+import { ref, onMounted, nextTick } from "vue";
+import * as echarts from "echarts";
+
+const chartRef = ref(null);
+
+onMounted(() => {
+    nextTick(() => {
+        const chart = echarts.init(chartRef.value);
+        chart.setOption({
+            title: { text: "Пример графика" },
+            tooltip: {},
+            xAxis: { type: "category", data: ["Пн", "Вт", "Ср", "Чт", "Пт"] },
+            yAxis: { type: "value" },
+            series: [{ name: "Данные", type: "line", data: [120, 200, 150, 80, 70] }],
+        });
+    });
+});
+</script>
+
+<template>
+    <div ref= "chartRef" style= "width: 100%; height: 400px;"></div>
+</template>
